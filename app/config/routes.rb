db94a4ae6d5383c99d8a5ac7809a2c7a64965eb0
yumekiti <yumekiti120440@gmail.com>
2022-03-23 00:06:03 +0900

    `fix` route

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
+  get    'tasks'     => 'tasks#index'
+  post   'tasks'     => 'tasks#store'
+  get    'tasks/:id' => 'tasks#show'
+  put    'tasks/:id' => 'tasks#update'
+  delete 'tasks/:id' => 'tasks#destroy'
 end
