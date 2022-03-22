056e2d9d756e29f59d3bde439272f442d60fdcce
yumekiti <yumekiti120440@gmail.com>
2022-03-23 00:04:18 +0900

    `add` post

--- app/app/controllers/tasks_controller.rb
+++ app/app/controllers/tasks_controller.rb
@@ -3,4 +3,13 @@ class TasksController < ApplicationController
     @tasks  = Task.order('limit_date').all
     @status = ['todo', 'doing', 'done']  
   end
+
+  def store
+    task = Task.new
+    task.task       = params[:task]
+    task.state      = params[:state]
+    task.limit_date = params[:limit_date]
+    task.save
+    redirect_to '/tasks', notice: 'タスクを作成しました。'
+  end
 end
